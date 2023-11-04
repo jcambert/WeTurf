1c564c779de603e7aeb9cb5f08b23691c2002f81
Ambert Jean-Christophe <jc.ambert@free.fr>
2023-11-04 17:10:42 +0100

    buidls props updates

--- libraries/We.AbpExtensions/nuget/Nuget_deploy.cmd
+++ libraries/We.AbpExtensions/nuget/Nuget_deploy.cmd
@@ -1,4 +1,3 @@
 rem Ouvrir un terminal dans VS Studio a partir de la racine du projet
 MSBuild.exe /t:pack /p:Configuration=Release
-nuget add bin\release\We.Utilities.1.0.2.nupkg -source D:\source\nuget_repo
-
+nuget add bin\release\We.AbpExtensions.1.0.2.nupkg -source D:\source\nuget_repo
--- libraries/We.Result/nuget/Nuget_deploy.cmd
+++ libraries/We.Result/nuget/Nuget_deploy.cmd
@@ -1,2 +1,3 @@
+rem Ouvrir un terminal dans VS Studio a partir de la racine du projet
 MSBuild.exe /t:pack /p:Configuration=Release
-nuget add bin\release\We.Results.1.0.0.nupkg -source e:\projets\nuget_repo
+nuget add bin\release\We.Results.1.0.2.nupkg -source D:\source\nuget_repo
